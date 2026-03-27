d5a3234529834da71f593cc076494232274f1220
Jean Philippe Koye <2klebest@gmail.com>
2026-03-27 12:28:21 -0400

    Ajout et Implemenation de la classe pays

---
+++ TP2-JPK_AD/src/livres/Pays.java
@@ -0,0 +1,43 @@
+package livres;
+
+public class Pays {
+
+    private String nom_pays;
+    private String code_pays;
+
+    public Pays() {
+        setNom_pays(nom_pays);
+        setCode_pays(code_pays);
+    }
+
+
+    public String getNom_pays() {
+        return nom_pays;
+    }
+
+    public void setNom_pays(String nom_pays) {
+        this.nom_pays = nom_pays;
+    }
+
+    public String getCode_pays() {
+        return code_pays;
+    }
+
+    public void setCode_pays(String code_pays) {
+         nom_pays.substring(0,3);
+         nom_pays.toUpperCase();
+         code_pays = nom_pays;
+        VerifierCodePays(code_pays);
+    }
+
+    public void VerifierCodePays(String code_pays) {
+
+
+        code_pays.toUpperCase();
+
+        if (!(code_pays.length() == 3)) {
+            throw new IllegalArgumentException("Code invalide (doit contenir 3 lettres)");
+
+        }
+    }
+}
